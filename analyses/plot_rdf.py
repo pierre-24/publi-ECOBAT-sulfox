173340620fca5779338e2261f45241c9c5e6212e
Pierre Beaujean <pierreb24@gmail.com>
2024-11-27 15:25:48 +0100

    improve RDF

--- analyses/plot_rdf.py
+++ analyses/plot_rdf.py
@@ -2,27 +2,44 @@ import pandas
 import matplotlib.pyplot as plt
 import numpy
 import argparse
+import scipy
 
 def prepare_data(data: pandas.DataFrame):
     return data
-        
+       
 
-def plot_system(ax, data: pandas.DataFrame, annotations: list = [], limits: tuple = (0, 10), label: str='', color: str=''):
+def plot_system(ax, data: pandas.DataFrame, limits: tuple = (0, 10), label: str='', color: str='', distance: int = 15, shiftmax: tuple=(10, 0), shiftmin=(10,-10)):
     ax.plot(data['r'], data['rdf'], '-', label=label, color=color)
+    
+    maxima = scipy.signal.find_peaks(data['rdf'], distance=distance, prominence=2)
+    minima = scipy.signal.argrelmin(data['rdf'].to_numpy(), order=distance)
+    
+    ax.text(.02, .9, label, transform=ax.transAxes, fontsize=12)
+    
+    #ax.plot(data['r'][maxima[0]], data['rdf'][maxima[0]], 'o', color=color)
+    #ax.plot(data['r'][minima[0]], data['rdf'][minima[0]], 'x', color=color)
+    for i in maxima[0]:
+        ax.annotate(
+            '{:.2f} ({:.1f})'.format(data['r'][i], data['cdf'][i]), 
+            (data['r'][i], data['rdf'][i]), 
+            shiftmax, 
+            textcoords='offset pixels', 
+            arrowprops={'color': color, 'arrowstyle': '->'}, 
+            color=color,
+            ha='right'
+        )
+    for i in minima[0]:
+        ax.annotate(
+            '{:.2f} ({:.1f})'.format(data['r'][i], data['cdf'][i]), 
+            (data['r'][i], data['rdf'][i]), 
+            shiftmin, 
+            textcoords='offset pixels', 
+            arrowprops={'color': color, 'arrowstyle': '->'}, 
+            color=color,
+            va='top',
+            ha='right'
+        )
     ax.set_xlim(*limits)
-    
-    max_rdf = data['rdf'].max()
-    
-    for ab,ae,x in annotations:
-        subdata = data[(data['r'] >= ab) & (data['r'] <= ae)]
-        max_ = data.iloc[subdata.idxmax()['rdf']]
-        ax.plot(max_['r'], max_['rdf'], 'o', color=color)
-        ax.annotate('{:.2f}'.format(max_['r']), (max_['r'], max_['rdf']), color=color, textcoords='offset pixels', xytext=(0, 5), ha='center', fontsize=8)
-        ax.plot([ab, ae], [max_['rdf'] * x] * 2, '-', color=color, linewidth=.7)
-        ax.plot([ab, ab], [0, max_['rdf'] * x], '-', color=color, linewidth=.7)
-        ax.plot([ae, ae], [max_['rdf'] * x - max_rdf * .05, max_['rdf'] * x + max_rdf * .05], '-', color=color, linewidth=.7)
-        ax.annotate('N={:.2f}'.format(subdata.iloc[-1]['cdf'] - subdata.iloc[0]['cdf']), (ae, max_['rdf'] * x), color=color, textcoords='offset pixels', xytext=(5, 0), ha='left', va='center')
-        
 
 
 def get_limits(inp: str):
@@ -57,29 +74,22 @@ parser = argparse.ArgumentParser()
 parser.add_argument('inputs', nargs='*')
 parser.add_argument('-l', '--limits', default='0:10', type=get_limits)
 parser.add_argument('-o', '--output', default='Data_rdf.pdf')
-parser.add_argument('-a', '--annotations', nargs='*', required=True, type=get_annotations)
 parser.add_argument('-n', '--names', nargs='*', required=True)
 
 args = parser.parse_args()
 
-if len(args.annotations) != len(args.inputs):
-    raise Exception('len(annotations) and len(inputs) should match')
-
 if len(args.names) != len(args.inputs):
     raise Exception('len(annotations) and len(inputs) should match')
 
-figure = plt.figure(figsize=(7, 5))
-ax1 = figure.subplots()
+figure = plt.figure(figsize=(9, 3 * len(args.inputs)))
+axes = figure.subplots(len(args.inputs), sharex=True)
 
-COLORS = ['red', 'green', 'blue']
-
-for i, (inp, name, annotations) in enumerate(zip(args.inputs, args.names, args.annotations)):
+for i, (ax, inp, name) in enumerate(zip(axes, args.inputs, args.names)):
     data = prepare_data(pandas.read_csv(inp))
-    plot_system(ax1, data, annotations=annotations, limits=args.limits, label=name, color=COLORS[i])
+    plot_system(ax, data, limits=args.limits, label=name, color='k', shiftmax=(-20, 5), shiftmin=(-20, -5))
 
-ax1.set_xlabel('r (Å)')
-ax1.set_ylabel('g(r)')
-ax1.legend()
+axes[-1].set_xlabel('r (Å)')
+[ax.set_ylabel('g(r)') for ax in axes.flatten()]
 
 plt.tight_layout()
 figure.savefig(args.output)
